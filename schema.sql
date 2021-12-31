80e17f1ed67d4c5486e2bd45207b99e20bc43202
mieltn <mike.elyutin@gmail.com>
2021-12-31 18:01:07 +0300

    updated datatypes

--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE crypto_rates (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     crypto_name VARCHAR(50) NOT NULL,
-    rate NUMERIC(50) NOT NULL,
-    market_cap NUMERIC(100),
+    rate NUMERIC(50, 4) NOT NULL,
+    market_cap NUMERIC(100) NOT NULL,
     currency VARCHAR(50) DEFAULT 'USD',
     created_at TIMESTAMP(0) DEFAULT now()
 );
